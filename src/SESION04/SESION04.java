bcb93dbef235608f3b7c2340023bbecb96466394
Eduja31 <167640327+Eduja31@users.noreply.github.com>
2024-04-20 14:19:10 -0500

    Add files via upload

---
+++ src/SESION04/SESION04.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package SESION04;
+import SESION04.pantallas.PantallaCalculadora;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SESION04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        PantallaCalculadora pantalla = new PantallaCalculadora();
+        pantalla.setVisible(true);
+        
+        
+        
+    }
+}
